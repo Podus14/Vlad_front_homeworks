b5be321f79931d2a6fa0dda409f663d78e6929eb
Podus14 <podus.leha@gmail.com>
2024-09-11 18:00:07 +0300

    fixed comments

--- homework_js/homework_js_2/homework.js
+++ homework_js/homework_js_2/homework.js
@@ -1,6 +1,6 @@
 // реализовать функцию createMultiplier, которая принимает один аргумент и возвращает функцию, которая умножает его на число
 const createMultiplier = (number) => {
-    return (mulNumber) => (number * mulNumber);
+    return mulNumber => number * mulNumber;
 };
 const multiplyBy3 = createMultiplier(3);
 console.log("task 1: ");
@@ -42,46 +42,42 @@ console.log(account.checkBalance()); // 120
 // и gender равен male
 // функция makePerson должна возвращать объект с такими полями: fullName, age, gender, canDrive
 // аргумент колбек функции для fullName должен иметь дефолтное значение - функцию, любую
-const fullNamePerson = (firstName = "Где", lastName = "имя??") => {
+const getFullName = (firstName, lastName) => {
     return firstName + " " + lastName;
 }
 const canDrive = (age, gender) => {
-    if (age >18 && gender === "male") {
-        return true;
-    }
-    return false;
+    if (age >= 18 && gender === "male") return true;
 }
 
 const person = {
     firstName:  "Alex",
     lastName: "Podus",
-    age: 17,
+    age: 22,
     gender: "male"
 }
 
-const makePerson = (person, canDriveCb, getFullNameCb) => {
+const makePerson = (person, canDriveCb, getFullNameCb = (firstName, lastName) => (firstName + " " + lastName).toUpperCase()) => {
+    const fullName = getFullNameCb(person.firstName, person.lastName);
+    const canDrive = canDriveCb(person.age, person.gender);
     return {
-        fullName: () => getFullNameCb(person.firstName, person.lastName),
+        fullName,
         age: person.age,
         gender: person.gender,
-        canDrive: () => canDriveCb(person.age, person.gender)
+        canDrive
     }
 };
 
-const personAlex = makePerson(person, canDrive, fullNamePerson);
+const personAlex = makePerson(person, canDrive);
 console.log("task 3: ");
-console.log(personAlex.fullName());
+console.log(personAlex.fullName);
 console.log(personAlex.age);
 console.log(personAlex.gender);
-console.log(personAlex.canDrive());
+console.log(personAlex.canDrive);
 // реализовать рекурсивную функцию countup, которая принимает аргумент initValue, и finishValue, и выводит в консоль числа от initValue до finishValue
 const countup = (initValue, finishValue) => {
-    if (initValue <= finishValue) {
-        console.log(initValue);
-        initValue++;
-        countup(initValue, finishValue);
-    }
-    return false;
+    if (initValue > finishValue) return;
+    console.log(initValue)
+    countup(initValue + 1, finishValue);
 }
 console.log("task 4: ");
 countup(1, 20);
